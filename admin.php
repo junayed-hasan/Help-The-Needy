6ff100042e0a1d675eab7f8d0616faf5411f625a
Junayed Hasan <junayedhasan100@gmail.com>
2022-07-03 23:40:01 +0600

    Started working on admin.php page.

---
+++ admin.php
@@ -0,0 +1,86 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Admin Panel</title>
+	<link rel = "stylesheet" type="text/css" href = "css/admin_style.css" >
+
+	<style type = "text/css">
+
+    </style>
+	
+	<script src="https://kit.fontawesome.com/d06b75c887.js" crossorigin="anonymous"></script>
+</head>
+
+<div class = "everything">
+<body>
+<header id = "info_header">
+		<div class = "area">
+			<h5>Email: htneedytoday@gmail.com | Contact: +8801852828239, +88029119298</h5>
+		</div>
+	</header>
+
+	<div class = "img">
+		<header id = "main_header">
+			<a href = "index.html" id = "logo"><img src = "images/logo2.png"></a>
+			<div class = "area2">
+				<h1><a href = "index.html">Help The Needy</a></h1>
+			</div>
+		</header>
+	</div>
+
+	<nav id = "navigation_bar">
+		<div class = "area">
+			<ul>
+				<li><a href = "index.html">Home</a></li>
+				<li><a href = "about.html">About</a></li>
+				<li><div class="dropdown">
+  					<a href = "#">Register↓</a>
+  					<div class="dropdown-content">
+  						<a href="donor_regi.html">Donor</a>
+  						<a href="recepient_regi.html">Recepient</a>
+  					</div>
+				</div></li>
+				<li><a href = "donateNow.html">Donate-Now</a></li>
+				<li><a href = "http://localhost/Project/admin.php">Admin-Panel</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<div id = "img">
+
+	</div>
+	
+	<div class = "container">
+		<section id = "showcase">
+			<h1>The Admin Panel</h1>
+		</div>
+	</section>
+
+
+	<footer>
+		<div class = "footer1">
+			<h4><b>Help The Needy</b></h5>
+			<h5>Donate today to make the world a better place.<br></h6>
+				<h3><br>Mohakhali DOHS, Dhaka 1212, Bangladesh</h3>
+				<h6><i class="fas fa-phone-alt"></i> +880-1852828239, +880-2-9119298  | <i class="fas fa-fax"></i>  +880-2-55668202</h6>
+				<h6><i class="fas fa-envelope"></i> htneedytoday@gmail.com</h6>
+		</div>
+
+		<div class = "footer2">
+				  <a href="https://www.facebook.com/Help-the-NEEDY-267511207225720" target = “_blank”><i class="fab fa-facebook"></i></a>
+					<a href="https://twitter.com/HelpingNeedy" target = “_blank”><i class="fab fa-twitter"></i></a>
+					<a href="https://www.youtube.com/channel/UCC2e1eBZgmQigHjiF7_pWhw" target = “_blank”><i class="fab fa-google-plus-g"></i></a> 
+					<a href="https://www.instagram.com/__help_the_needy__/" target = “_blank”><i class="fab fa-instagram"></i></a>
+					<a href="https://www.youtube.com/channel/UCC2e1eBZgmQigHjiF7_pWhw" target = “_blank”><i class="fab fa-youtube"></i></a>
+
+				<h4><br><br>Developed & Maintained by IT Office, HTN</h4>
+				<h4>© 2021 Help The Needy. All rights reserved.</h4>
+		</div>
+		
+	</footer>
+
+</div>
+</body>
+</div>
+</html>
