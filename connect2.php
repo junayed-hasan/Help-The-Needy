d6fff9ec23ba5533d64fd34fe480cabbd896c123
Junayed Hasan <junayedhasan100@gmail.com>
2022-08-04 23:19:51 +0600

    Added table attributes as variables for quering in php connection file.

--- connect2.php
+++ connect2.php
@@ -5,4 +5,11 @@
 	$dbname = "lab_project";
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	$name = $_POST['rname'];
+	$email = $_POST['remail'];
+	$address = $_POST['raddress'];
+	$phone = $_POST['rphone'];
+	$city = $_POST['rcity'];
+
 ?>
